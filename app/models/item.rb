49e077f421a23839f464b7eec96588b29c7ef82f
seedsat <e.satoshi.bi@gmail.com>
2021-05-28 13:05:15 +0900

    ビューとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :info, length: { maximum: 1000 }
+    validates :category_id
+    validates :deliverycharge_id
+    validates :prefecture_id
+    validates :shippingday_id
+    validates :price
+  end
 end
--- db/migrate/20210528033450_create_items.rb
+++ db/migrate/20210528033450_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name, null: false
-      t.text :info, null: false
-      t.integer :category_id, null: false
-      t.integer :itemstatus_id, null: false
-      t.integer :deliverycharge_id, null: false
-      t.integer :prefecture_id, null: false
-      t.integer :shippingday_id, null: false
-      t.integer :price, null: false
+      t.string     :name, null: false
+      t.text       :info, null: false
+      t.integer    :category_id, null: false
+      t.integer    :itemstatus_id, null: false
+      t.integer    :deliverycharge_id, null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :shippingday_id, null: false
+      t.integer    :price, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
