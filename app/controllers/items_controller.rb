4a31c3825a7a15ce61bb512b92c6ed5edf2388f8
seedsat <e.satoshi.bi@gmail.com>
2021-05-29 22:33:23 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at desc')
@@ -23,9 +24,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
@@ -33,5 +41,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :itemstatus_id, :deliverycharge_id, :prefecture_id,
                                  :shippingday_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_index
+    @item = Item.find(params[:id])
+    redirect_to root_path unless user_signed_in? && @item.user.id == current_user.id
   end
 end
