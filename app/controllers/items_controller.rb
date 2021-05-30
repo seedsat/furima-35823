79f0ea15307b6fae8578727983f5008502420f08
seedsat <e.satoshi.bi@gmail.com>
2021-05-30 14:35:16 +0900

    削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if @item.user.id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
