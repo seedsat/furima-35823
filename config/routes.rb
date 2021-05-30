b359caa7b3d2e296e247442a4168eb84277b57a1
seedsat <e.satoshi.bi@gmail.com>
2021-05-30 16:22:43 +0900

    コントローラー、ルーティング、ビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'purchases/index'
+  get 'purchases/new'
   root to: 'items#index'
   devise_for :users
-  resources :items
+  resources :items do
+    resources :purchases, only: [:index, :create]
+  end
 end
