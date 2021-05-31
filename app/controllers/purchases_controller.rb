32048d1fdce70a1c5e5e62df058a4159b846fdd7
seedsat <e.satoshi.bi@gmail.com>
2021-05-31 11:45:38 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,35 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :find_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
   end
 
   def create
+    @purchase = Purchase.new(user_id: current_user.id, item_id: params[:item_id])
+    @token = params[:token]
+    unless  @token.nil?
+      purchase_pay
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_pay
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: @token,
+      currency: 'jpy'
+    )
   end
 end
