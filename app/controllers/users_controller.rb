65accfec1d04cc0ca66f37a396751419779ffdaf
seedsat <e.satoshi.bi@gmail.com>
2021-06-09 10:37:57 +0900

    userが存在しない時はトップにリダイレクト処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :users_item, only: :show
+  before_action :move_index_user_nil, only: :show
   before_action :move_index, only: :show
 
   def show
@@ -8,10 +9,14 @@ class UsersController < ApplicationController
   private
 
   def users_item
-    @user = User.find(params[:id]);
+    @user = User.find_by_id(params[:id])
   end
 
   def move_index
     redirect_to root_path unless current_user.id == @user.id
+  end
+
+  def move_index_user_nil
+    redirect_to root_path if @user.nil?
   end
 end
