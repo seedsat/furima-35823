0d175a608e656dc7120ee4f28eed9f1a9c8aa9df
seedsat <e.satoshi.bi@gmail.com>
2021-06-10 13:46:51 +0900

    コメント機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    text {Faker::Lorem.sentence}
   end
 end
