d058073ef768fad2bde6e2b4c3d09a63fe91f8fb
seedsat <e.satoshi.bi@gmail.com>
2021-06-10 14:37:32 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :items do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destory]
     resources :purchases, only: [:index, :create]
     collection do
       get 'search'
     end
+    resources :likes
   end
   resources :users, only: [:show, :edit, :update]
 end
